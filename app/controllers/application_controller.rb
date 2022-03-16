9356c86c256841e6cdac0dcfa02569f999a7b57c
wendyliwinter2022 <wendy.li1@kellogg.northwestern.edu>
2022-03-16 04:26:26 +0000

    nav setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,9 @@ class ApplicationController < ActionController::Base
   before_action :current_user
 
   def current_user
-    puts "------------------ code before every request ------------------"
+    if session[:user_id]
+     
+      @current_user = User.find(session[:user_id])
+    end
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,28 @@ class SessionsController < ApplicationController
   end
 
   def create
+    def create
+      @user = User.where({ email: params["email"] })[0]
+      if @user
+        if BCrypt::Password.new(@user.password) == params["password"]
+          session[:user_id] = @user.id
+          flash[:notice] = "Welcome."
+          redirect_to "/places"
+        else
+          flash[:notice] = "Failed login. Try again."
+          redirect_to "/sessions/new"
+        end
+      else
+        flash[:notice] = "Failed login. Try again."
+        redirect_to "/sessions/new"
+      end
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    flash[:notice] = "Goodbye."
+    redirect_to "/sessions/new"
   end
 end
   
