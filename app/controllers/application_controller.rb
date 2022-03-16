ee8ba84470daceede3ce0a4dbc91b27e000a01bb
wendyliwinter2022 <wendy.li1@kellogg.northwestern.edu>
2022-03-16 05:36:15 +0000

    forms

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,8 @@ class ApplicationController < ActionController::Base
   before_action :current_user
 
   def current_user
-    if session[:user_id]
-     
+    if session[:user_id]  
+      #session[:user_id] = nil
       @current_user = User.find(session[:user_id])
     end
   end
